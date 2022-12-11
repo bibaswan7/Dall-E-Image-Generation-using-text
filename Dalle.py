d5b771cdffb9c6f527f776a874e83c99f4a14b11
Bibaswan Prasai <59719358+bibaswan7@users.noreply.github.com>
2022-12-11 21:23:53 +0545

    Add files via upload

---
+++ Dalle.py
@@ -0,0 +1,33 @@
+import openai
+import urllib.request
+from PIL import Image
+import cv2
+import streamlit as st
+
+openai.api_key = "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"
+
+def generate_image(image_description):
+    
+    img_response = openai.Image.create(
+        prompt = image_description,
+        n=1,
+        size="512x512")
+    
+    img_url = img_response['data'][0]['url']
+    
+    urllib.request.urlretrieve(img_url, "img.png")
+    
+    img = Image.open("img.png")
+    
+    return img
+
+
+st.title('DALL.E - Image Generation - OpenAI')
+
+img_description = st.text_input('Image Description')
+
+if st.button('Generate Image'):
+    generated_img = generate_image(img_description)
+    st.image(generated_img)
+    
+    
